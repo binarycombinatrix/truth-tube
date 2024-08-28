9f9e4fae4d149c77c8b74e7b58591a77e2c8a12a
raj <panchalraj36@gmail.com>
2024-08-28 13:05:22 +0530

    logout from channel

--- pages/channel/[path].tsx
+++ pages/channel/[path].tsx
@@ -5,6 +5,7 @@ import { generateClient } from "aws-amplify/data"
 import type { Schema } from "@/amplify/data/resource"
 import { StorageImage } from "@aws-amplify/ui-react-storage"
 import { VideoCard } from "@/components/videocard"
+import Router from "next/router"
 
 const client = generateClient<Schema>()
 
@@ -84,27 +85,57 @@ export const getStaticProps: GetStaticProps<VideoProps> = async ({
   }
 }
 export default function App({ data, path }: VideoProps) {
+  const logout = () => {
+    localStorage.clear()
+    Router.push("/")
+  }
+
   return (
     <main>
-      {!path && (
-        <div className="title">
-          <h1>Please login and setup your channel</h1>
-        </div>
-      )}
       <div className="channel-profile">
-        {data?.[0]?.dp ? (
-          <StorageImage
-            className="card-dp"
-            path={data[0].dp ?? ""}
-            fallbackSrc="/profile.svg"
-            alt="Profile"
-          />
+        {path == "null" ? (
+          <div className="title">
+            <h1>Please login and setup your channel</h1>
+          </div>
         ) : (
-          <Link href="/login">
-            <img className="card-dp" src="/profile.svg" alt="login" />
-          </Link>
+          <>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+            >
+              {data?.[0]?.dp ? (
+                <StorageImage
+                  className="card-dp"
+                  path={data[0].dp ?? ""}
+                  fallbackSrc="/profile.svg"
+                  alt="Profile"
+                />
+              ) : (
+                <Link href="/login">
+                  <img className="card-dp" src="/profile.svg" alt="login" />
+                </Link>
+              )}
+
+              <h1>{data?.[0]?.dn}</h1>
+            </div>
+          </>
         )}
-        <h1>{data?.[0]?.dn}</h1>
+        <button
+          style={{
+            cursor: "pointer",
+            display: "flex",
+            gap: "0.5rem",
+            alignItems: "center",
+          }}
+          onClick={logout}
+        >
+          <Image src="/logout.svg" alt="search" width={30} height={30} />
+          <span>Sign Out</span>
+        </button>
       </div>
       <ul className="video-list">
         {data &&

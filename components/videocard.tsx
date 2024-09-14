8cc04b745ef3db232382707849b5e18eef891d25
raj <panchalraj36@gmail.com>
2024-09-14 08:48:11 +0530

    signup and channel

--- components/videocard.tsx
+++ components/videocard.tsx
@@ -1,19 +1,19 @@
-import { VideoObject } from "@/types"
-import { StorageImage } from "@aws-amplify/ui-react-storage"
-import Link from "next/link"
+import { VideoObject } from '@/types'
+import { StorageImage } from '@aws-amplify/ui-react-storage'
+import Link from 'next/link'
 
 export const VideoCard = ({ video }: { video: VideoObject }) => {
   return (
     <li key={video.sortKey} className="video-card">
       {video.thumbnail && (
         <div className="thumbnail-container">
-          <Link href={`/video/${video.path ?? ""}`}>
+          <Link href={`/video/${video.path ?? ''}`}>
             <StorageImage
               path={video.thumbnail}
               // width={400}
               className="thumbnailImage"
               // height={400}
-              alt={video?.sortKey?.split("_")[0]}
+              alt={video?.sortKey?.split('_')[0]}
             />
           </Link>
         </div>
@@ -21,22 +21,15 @@ export const VideoCard = ({ video }: { video: VideoObject }) => {
       <div className="card-details">
         <span>
           {video?.dp ? (
-            <StorageImage
-              className="card-dp"
-              path={video.dp ?? ""}
-              fallbackSrc="/profile.svg"
-              alt="Profile"
-            />
+            <StorageImage className="card-dp" path={video.dp ?? ''} fallbackSrc="/profile.svg" alt="Profile" />
           ) : (
             <img className="card-dp" src="/profile.svg" alt="profile" />
           )}
         </span>
         <span>
-          <div className="card-title">
-            {video?.title ?? video?.sortKey?.split(/[_#]/)[1]}
-          </div>
-          <Link href={`/channel/${video.channel ?? ""}`}>
-            <div className="card-channel">{video?.dn ?? ""}</div>
+          <div className="card-title">{video?.title ?? video?.sortKey?.split(/[_#]/)[1]}</div>
+          <Link href={`/channel/${video?.sortKey?.split(/[_]/)[1] ?? ''}`}>
+            <div className="card-channel">{video?.dn ?? ''}</div>
           </Link>
         </span>
       </div>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,28 +1,29 @@
-"use client"
-import type { ChangeEvent, FormEvent, MouseEvent } from "react"
-import { useState, useEffect } from "react"
-import Router from "next/router"
-import { v1 as uuidv1 } from "uuid"
-import { signIn } from "aws-amplify/auth"
-import { generateClient } from "aws-amplify/data"
-import { type Schema } from "../amplify/data/resource"
-import Toast from "../components/toast"
-import { uploadData } from "aws-amplify/storage"
-import { StorageImage } from "@aws-amplify/ui-react-storage"
+'use client'
+import type { ChangeEvent, FormEvent, MouseEvent } from 'react'
+import { useState, useEffect } from 'react'
+import Router from 'next/router'
+import { v1 as uuidv1 } from 'uuid'
+import { signIn } from 'aws-amplify/auth'
+import { generateClient } from 'aws-amplify/data'
+import { type Schema } from '../amplify/data/resource'
+import Toast from '../components/toast'
+import { uploadData } from 'aws-amplify/storage'
+import { StorageImage } from '@aws-amplify/ui-react-storage'
+import Link from 'next/link'
 
 const client = generateClient<Schema>()
 
 export default function Login() {
-  const [message, setMessage] = useState("")
-  const [type, setType] = useState<"success" | "error" | "info">("success")
+  const [message, setMessage] = useState('')
+  const [type, setType] = useState<'success' | 'error' | 'info'>('success')
   const [showProfile, setShowProfile] = useState(false)
   const [dp, setDp] = useState<File | null>(null)
-  const [dpPath, setDpPath] = useState<string>("")
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
+  const [dpPath, setDpPath] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
   const [profile, setProfile] = useState({
-    dn: "",
-    description: "",
+    dn: '',
+    description: ''
   })
 
   // const handleNewUserProfile = async () => {
@@ -139,18 +140,18 @@ export default function Login() {
       try {
         const uploadres = await uploadData({
           path: ({ identityId }) => `dp/${identityId}/${dp.name}`,
-          data: dp,
+          data: dp
         })
-        const fpath = (await uploadres?.result)?.path ?? ""
+        const fpath = (await uploadres?.result)?.path ?? ''
         setDpPath(fpath)
-        console.log("Upload result:", uploadres)
-        alert("Upload successful!" + fpath)
+        console.log('Upload result:', uploadres)
+        alert('Upload successful!' + fpath)
       } catch (error) {
-        console.error("Upload failed:", error)
-        alert("Upload failed. Please try again.")
+        console.error('Upload failed:', error)
+        alert('Upload failed. Please try again.')
       }
     } else {
-      alert("Please select a file to upload.")
+      alert('Please select a file to upload.')
     }
   }
 
@@ -158,127 +159,118 @@ export default function Login() {
     try {
       const signInResult = await signIn({
         username: email,
-        password: password,
+        password: password
       })
 
-      console.log("sign in outcome=>`", signInResult)
+      console.log('sign in outcome=>`', signInResult)
 
       if (signInResult.isSignedIn === true) {
-        localStorage.setItem("email", email)
+        localStorage.setItem('email', email)
 
         const { errors: err, data: old_user } = await client.models.Video.get({
           partitionKey: email,
-          sortKey: email,
+          sortKey: email
         })
 
         if (err) {
-          console.log("error in getting old user", err)
+          console.log('error in getting old user', err)
         }
         if (old_user) {
-          console.log("user found", old_user)
+          console.log('user found', old_user)
           if (old_user.username && old_user.dn && old_user.dp) {
-            localStorage.setItem("username", old_user.username)
-            localStorage.setItem("dn", old_user.dn)
-            localStorage.setItem("dp", old_user.dp)
-            old_user.subbedto &&
-              localStorage.setItem(
-                "subbedto",
-                JSON.stringify(old_user.subbedto)
-              )
-            old_user.likedTo &&
-              localStorage.setItem("likedTo", JSON.stringify(old_user.likedTo))
+            localStorage.setItem('username', old_user.username)
+            localStorage.setItem('dn', old_user.dn)
+            localStorage.setItem('dp', old_user.dp)
+            old_user.subbedto && localStorage.setItem('subbedto', JSON.stringify(old_user.subbedto))
+            old_user.likedTo && localStorage.setItem('likedTo', JSON.stringify(old_user.likedTo))
             setProfile({
               dn: old_user.dn,
-              description: old_user.description ?? "",
+              description: old_user.description ?? ''
             })
             setDpPath(old_user.dp)
           }
 
-          setType("success")
-          setMessage("Signed in successfully")
+          setType('success')
+          setMessage('Signed in successfully')
           setShowProfile(true)
-          setPassword("")
+          setPassword('')
         } else {
-          const { errors: err, data: new_user } =
-            await client.models.Video.create(
-              {
-                partitionKey: email,
-                sortKey: email,
-                type: "user_profile",
-                subbedto: [],
-                likedTo: [],
-              },
-              {
-                authMode: "userPool",
-              }
-            )
+          const { errors: err, data: new_user } = await client.models.Video.create(
+            {
+              partitionKey: email,
+              sortKey: email,
+              type: 'user_profile',
+              subbedto: [],
+              likedTo: []
+            },
+            {
+              authMode: 'userPool'
+            }
+          )
 
           if (err) {
-            console.log("error in setting up user", err)
-            setType("error")
-            setMessage("Error setting up user please login again")
+            console.log('error in setting up user', err)
+            setType('error')
+            setMessage('Error setting up user please login again')
             localStorage.clear()
           }
           if (new_user) {
-            console.log("user created", new_user)
-            localStorage.setItem("subbedto", JSON.stringify(new_user.subbedto))
-            localStorage.setItem("likedTo", JSON.stringify(new_user.likedTo))
+            console.log('user created', new_user)
+            localStorage.setItem('subbedto', JSON.stringify(new_user.subbedto))
+            localStorage.setItem('likedTo', JSON.stringify(new_user.likedTo))
             setShowProfile(true)
-            setType("info")
-            setMessage("Please setup your profile")
-            setPassword("")
+            setType('info')
+            setMessage('Please setup your profile')
+            setPassword('')
           }
         }
       }
     } catch (err) {
-      setType("error")
-      setMessage("Sign in error: " + err)
-      console.log("sign in error=>`", err)
+      setType('error')
+      setMessage('Sign in error: ' + err)
+      console.log('sign in error=>`', err)
     }
   }
 
   const updateProfileData = async () => {
     try {
-      if (email !== "" || localStorage.getItem("email")) {
-        const keyval =
-          email !== "" ? email : localStorage.getItem("email") ?? ""
-        const newUsername =
-          localStorage.getItem("username") ?? profile.dn + uuidv1()
+      if (email !== '' || localStorage.getItem('email')) {
+        const keyval = email !== '' ? email : localStorage.getItem('email') ?? ''
+        const newUsername = localStorage.getItem('username') ?? profile.dn + uuidv1()
 
         const { errors, data } = await client.models.Video.update(
           {
             partitionKey: keyval,
             sortKey: keyval,
-            type: "user_profile",
+            type: 'user_profile',
             dn: profile.dn,
             dp: dpPath,
             username: newUsername,
-            description: profile.description,
+            description: profile.description
           },
           {
-            authMode: "userPool",
+            authMode: 'userPool'
           }
         )
 
         if (errors) {
-          console.log("Error updating profile data:", errors)
-          setType("error")
-          setMessage("Error updating profile data: " + errors)
+          console.log('Error updating profile data:', errors)
+          setType('error')
+          setMessage('Error updating profile data: ' + errors)
         } else {
-          const { data: channelData, errors } =
-            await client.models.Video.create(
-              {
-                partitionKey: "channel",
-                sortKey: newUsername,
-                type: "channel_profile",
-                subs: [],
-                username: newUsername,
-                likes: "0",
-              },
-              {
-                authMode: "userPool",
-              }
-            )
+          const { data: channelData, errors } = await client.models.Video.create(
+            {
+              partitionKey: 'channel',
+              sortKey: newUsername,
+              type: 'channel_profile',
+              subs: [],
+              username: newUsername,
+              likes: '0'
+            },
+            {
+              authMode: 'userPool'
+            }
+          )
 
           // if (errors) {
           //   console.log("Error updating channel data:", errors)
@@ -286,16 +278,16 @@ export default function Login() {
           //   setMessage("Error updating channel data: " + errors)
           // }
           if (channelData) {
-            console.log("channel profile data added", channelData)
-            setType("success")
-            setMessage("Channel created successfully")
+            console.log('channel profile data added', channelData)
+            setType('success')
+            setMessage('Channel created successfully')
           }
 
-          localStorage.setItem("dp", dpPath)
-          localStorage.setItem("username", newUsername)
-          localStorage.setItem("dn", profile.dn)
-          console.log("Profile data updated:", data)
-          Router.push("/")
+          localStorage.setItem('dp', dpPath)
+          localStorage.setItem('username', newUsername)
+          localStorage.setItem('dn', profile.dn)
+          console.log('Profile data updated:', data)
+          Router.push('/')
           // setProfile({
           //   dn: "",
           //   description: "",
@@ -303,11 +295,11 @@ export default function Login() {
           // setDpPath("")
         }
       } else {
-        setType("error")
-        setMessage("Error updating profile data: Please re-login")
+        setType('error')
+        setMessage('Error updating profile data: Please re-login')
       }
     } catch (error) {
-      console.log("Error updating profile data:", error)
+      console.log('Error updating profile data:', error)
     }
   }
 
@@ -317,14 +309,7 @@ export default function Login() {
         <div className="upload-form">
           <div className="form-group">
             <label htmlFor="dn">Display Name:</label>
-            <input
-              type="text"
-              id="dn"
-              name="dn"
-              required
-              value={profile.dn}
-              onChange={(e) => setProfile({ ...profile, dn: e.target.value })}
-            />
+            <input type="text" id="dn" name="dn" required value={profile.dn} onChange={(e) => setProfile({ ...profile, dn: e.target.value })} />
           </div>
           <div className="form-group">
             <label htmlFor="description">About Channel</label>
@@ -334,39 +319,21 @@ export default function Login() {
               name="description"
               required
               value={profile.description}
-              onChange={(e) =>
-                setProfile({ ...profile, description: e.target.value })
-              }
+              onChange={(e) => setProfile({ ...profile, description: e.target.value })}
             />
           </div>
           <div className="form-group">
             <label htmlFor="dp">Profile Picture:</label>
-            <input
-              type="file"
-              id="dp"
-              name="dp"
-              onChange={handleDpInput}
-              required
-            />
+            <input type="file" id="dp" name="dp" onChange={handleDpInput} required />
 
             <div>
               <button type="button" onClick={uploadDp}>
                 Upload
               </button>
-              {dpPath && dpPath != "" && (
-                <StorageImage
-                  className="upload-preview"
-                  path={dpPath}
-                  alt="Profile Picture"
-                />
-              )}
+              {dpPath && dpPath != '' && <StorageImage className="upload-preview" path={dpPath} alt="Profile Picture" />}
             </div>
           </div>
-          <button
-            type="button"
-            className="submit-button"
-            onClick={updateProfileData}
-          >
+          <button type="button" className="submit-button" onClick={updateProfileData}>
             Update
           </button>
           {/* <div className="form-group"> */}
@@ -382,27 +349,25 @@ export default function Login() {
         <div onSubmit={handleSubmit} className="upload-form">
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <button type="button" onClick={handleSubmit}>
             Submit
           </button>
+          <div className="form-group">
+            <div></div>
+            <div>
+              <span>{`Don't have an account? `} </span>
+
+              <Link style={{ color: 'blue' }} href="/signup">
+                Sign Up
+              </Link>
+            </div>
+          </div>
         </div>
       )}
       {message && <Toast message={message} type={type} />}

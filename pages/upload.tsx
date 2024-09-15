33622bf90f59cfbb02dc852b97bc28bd45e7d83c
raj <panchalraj36@gmail.com>
2024-09-15 12:48:47 +0530

    upload loader

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,11 +1,11 @@
 // Upload successful! videos/ap-south-1:9576007c-a938-c03d-4615-dbc1b99bc041/RavanHeadReplace.mp4
-import { useState, ChangeEvent } from "react"
-import { v1 as uuidv1 } from "uuid"
-import { uploadData } from "aws-amplify/storage"
-import { generateClient } from "aws-amplify/data"
-import { type Schema } from "../amplify/data/resource"
-import { StorageImage } from "@aws-amplify/ui-react-storage"
-import Router from "next/router"
+import { useState, ChangeEvent } from 'react'
+import { v1 as uuidv1 } from 'uuid'
+import { uploadData } from 'aws-amplify/storage'
+import { generateClient } from 'aws-amplify/data'
+import { type Schema } from '../amplify/data/resource'
+import { StorageImage } from '@aws-amplify/ui-react-storage'
+import Router from 'next/router'
 
 const client = generateClient<Schema>()
 
@@ -24,22 +24,21 @@ interface CreateForm extends HTMLFormElement {
 }
 
 export default function Upload() {
-  const [debate, setDebate] = useState<Schema["Argument"]["type"]>({
-    content: "",
+  const [debate, setDebate] = useState<Schema['Argument']['type']>({
+    content: '',
     self: false,
-    dp: "",
-    dn: "",
-    links: [],
+    dp: '',
+    dn: '',
+    links: []
   })
-  const [link, setLink] = useState("")
-  const [debateArr, setDebateArr] = useState<Schema["Argument"]["type"][] | []>(
-    []
-  )
+  const [link, setLink] = useState('')
+  const [debateArr, setDebateArr] = useState<Schema['Argument']['type'][] | []>([])
   const [opFile, setOpFile] = useState<File | null>(null)
   const [file, setFile] = useState<File | null>(null) // Specify the type for the file state
   const [thumbnail, setThumbnail] = useState<File | null>(null)
-  const [filePath, setFilePath] = useState<string>("")
-  const [thumbnailPath, setThumbnailPath] = useState<string>("")
+  const [filePath, setFilePath] = useState<string>('')
+  const [thumbnailPath, setThumbnailPath] = useState<string>('')
+  const [loader, setLoader] = useState(false)
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setFile(event.target.files[0])
@@ -57,21 +56,21 @@ export default function Upload() {
     const newLinks = oldDebate.links ? [...oldDebate.links, link] : [link]
     setDebate({
       ...oldDebate,
-      links: newLinks,
+      links: newLinks
     })
-    setLink("")
+    setLink('')
   }
 
   const handleDebate = () => {
     const newDebateArr = [...debateArr, debate]
-    console.log("newDebateArr", newDebateArr)
+    console.log('newDebateArr', newDebateArr)
     setDebateArr(newDebateArr)
     setDebate({
-      content: "",
+      content: '',
       self: false,
-      dp: "",
-      dn: "",
-      links: [],
+      dp: '',
+      dn: '',
+      links: []
     })
     setOpFile(null)
   }
@@ -90,68 +89,77 @@ export default function Upload() {
 
   const handleUpload = async () => {
     if (file) {
+      setLoader(true)
       try {
         const uploadres = await uploadData({
           path: ({ identityId }) => `videos/${identityId}/${file.name}`,
-          data: file,
+          data: file
         })
 
-        const fpath = (await uploadres?.result)?.path ?? ""
-        console.log("Upload result:", uploadres)
-        alert("Upload successful!" + fpath)
+        const fpath = (await uploadres?.result)?.path ?? ''
+        console.log('Upload result:', uploadres)
+        alert('Upload successful!' + fpath)
         setFilePath(fpath)
       } catch (error) {
-        console.error("Upload failed:", error)
-        alert("Upload failed. Please try again.")
+        console.error('Upload failed:', error)
+        alert('Upload failed. Please try again.')
       }
     } else {
-      alert("Please select a file to upload.")
+      alert('Please select a file to upload.')
     }
+
+    setLoader(false)
   }
 
   const thumbnailUpload = async () => {
     if (thumbnail) {
+      setLoader(true)
       try {
         const uploadres = await uploadData({
           path: `thumbnails/${thumbnail.name}`,
-          data: thumbnail,
+          data: thumbnail
         })
 
-        const fpath = (await uploadres?.result)?.path ?? ""
-        console.log("Upload result:", uploadres)
-        alert("Upload successful!" + fpath)
+        const fpath = (await uploadres?.result)?.path ?? ''
+        console.log('Upload result:', uploadres)
+        alert('Upload successful!' + fpath)
         setThumbnailPath(fpath)
       } catch (error) {
-        console.error("Upload failed:", error)
-        alert("Upload failed. Please try again.")
+        console.error('Upload failed:', error)
+        alert('Upload failed. Please try again.')
       }
     } else {
-      alert("Please select a file to upload.")
+      alert('Please select a file to upload.')
     }
+
+    setLoader(false)
   }
 
   const handleOpUpload = async () => {
     if (opFile) {
+      setLoader(true)
       try {
         const uploadres = await uploadData({
           path: ({ identityId }) => `dp/${identityId}/${opFile.name}`,
-          data: opFile,
+          data: opFile
         })
         if (uploadres) {
-          const fpath = (await uploadres?.result)?.path ?? ""
+          const fpath = (await uploadres?.result)?.path ?? ''
           setDebate({ ...debate, dp: fpath })
-          alert("Upload successful!" + fpath)
-          console.log("the dp of the op is ", fpath)
+          alert('Upload successful!' + fpath)
+          console.log('the dp of the op is ', fpath)
         } else {
-          console.log("error in uploading dp: please reupload the file")
-          alert("Upload failed. Please try again.")
+          console.log('error in uploading dp: please reupload the file')
+          alert('Upload failed. Please try again.')
         }
       } catch (error) {
-        console.log("error in uploading dp: please reupload the file")
+        console.log('error in uploading dp: please reupload the file')
       }
     } else {
-      alert("Please select a file to upload.")
+      alert('Please select a file to upload.')
     }
+
+    setLoader(false)
   }
   const handleSubmit = async (event: React.FormEvent<CreateForm>) => {
     event.preventDefault()
@@ -165,91 +173,83 @@ export default function Upload() {
     const tUUID = uuidv1()
 
     try {
-      if (localStorage.getItem("username") && localStorage.getItem("dn")) {
+      if (localStorage.getItem('username') && localStorage.getItem('dn')) {
         const { errors, data: video } = await client.models.Video.create(
           {
-            partitionKey: "Educational",
-            sortKey: "v#" + title + "_" + localStorage.getItem("username"),
-            type: "video",
+            partitionKey: 'Educational',
+            sortKey: 'v#' + title + '_' + localStorage.getItem('username'),
+            type: 'video',
             thumbnail: thumbnailPath,
             url: filePath,
-            dp: localStorage.getItem("dp") ?? "",
-            dn:
-              localStorage.getItem("dn") ??
-              localStorage.getItem("username") ??
-              "",
+            dp: localStorage.getItem('dp') ?? '',
+            dn: localStorage.getItem('dn') ?? localStorage.getItem('username') ?? '',
             debate: debateArr,
-            title: tInp,
+            title: tInp
           },
           {
-            authMode: "userPool",
+            authMode: 'userPool'
           }
         )
 
-        if (errors) console.log(" Error1 in adding video data to DB:", errors)
+        if (errors) console.log(' Error1 in adding video data to DB:', errors)
         else {
-          console.log("New video added: |> ", video)
+          console.log('New video added: |> ', video)
         }
 
         if (video) {
           const { errors, data: user_video } = await client.models.Video.create(
             {
-              partitionKey:
-                "u#" + (localStorage.getItem("username") ?? "Educational"),
-              sortKey: "v#" + title + "_" + localStorage.getItem("username"),
-              category: "Educational",
-              type: "user",
+              partitionKey: 'u#' + (localStorage.getItem('username') ?? 'Educational'),
+              sortKey: 'v#' + title + '_' + localStorage.getItem('username'),
+              category: 'Educational',
+              type: 'user',
               thumbnail: thumbnailPath,
-              dp: localStorage.getItem("dp") ?? "",
-              dn:
-                localStorage.getItem("dn") ??
-                localStorage.getItem("username") ??
-                "",
-              title: tInp,
+              dp: localStorage.getItem('dp') ?? '',
+              dn: localStorage.getItem('dn') ?? localStorage.getItem('username') ?? '',
+              title: tInp
             },
             {
-              authMode: "userPool",
+              authMode: 'userPool'
             }
           )
-          console.log("Video data added:", user_video)
-          if (errors) console.log(" Error2 in adding video data to DB:", errors)
+          console.log('Video data added:', user_video)
+          if (errors) console.log(' Error2 in adding video data to DB:', errors)
 
           if (user_video) {
             alert(`Video uploaded successfully!`)
-            Router.push("/")
+            Router.push('/')
           }
         }
         // Handle form submission logic here
       } else {
-        throw new Error("Please create you channel or relogin")
+        throw new Error('Please create you channel or relogin')
       }
     } catch (error) {
-      console.log("Error in adding video data to DB:", error)
+      console.log('Error in adding video data to DB:', error)
     }
   }
 
   return (
     <div>
+      {loader && (
+        <div id="nprogress">
+          <div className="bar" role="bar">
+            <div className="peg"></div>
+          </div>
+          <div className="spinner" role="spinner">
+            <div className="spinner-icon"></div>
+          </div>
+        </div>
+      )}
       <form onSubmit={handleSubmit} className="upload-form">
         <div className="form-group">
           <label htmlFor="thumbnail">Upload Thumbnail:</label>
-          <input
-            type="file"
-            id="thumbnail"
-            onChange={handleThumbnailChange}
-            required
-          />
+          <input type="file" id="thumbnail" onChange={handleThumbnailChange} required />
           <div>
             <button type="button" onClick={thumbnailUpload}>
               Upload
             </button>
-            {thumbnailPath && thumbnailPath != "" && (
-              <StorageImage
-                className="upload-preview"
-                path={thumbnailPath}
-                alt="Thumbnail"
-              />
-            )}
+            {thumbnailPath && thumbnailPath != '' && <StorageImage className="upload-preview" path={thumbnailPath} alt="Thumbnail" />}
           </div>
         </div>
         <div className="form-group">
@@ -296,8 +296,8 @@ export default function Upload() {
               setDebate({
                 ...debate,
                 self: e.target.checked,
-                dp: localStorage.getItem("dp"),
-                dn: localStorage.getItem("dn"),
+                dp: localStorage.getItem('dp'),
+                dn: localStorage.getItem('dn')
               })
             }
           />
@@ -310,7 +310,7 @@ export default function Upload() {
                 id="dn"
                 name="dn" // Add name attribute for form.elements access
                 // required
-                value={debate.dn ?? ""}
+                value={debate.dn ?? ''}
                 onChange={(e) => setDebate({ ...debate, dn: e.target.value })}
               />
             </div>
@@ -323,13 +323,7 @@ export default function Upload() {
                 <button type="button" onClick={handleOpUpload}>
                   Upload Dp
                 </button>
-                {debate?.dp && debate?.dp != "" && (
-                  <StorageImage
-                    className="upload-preview"
-                    path={debate.dp}
-                    alt="Dp"
-                  />
-                )}
+                {debate?.dp && debate?.dp != '' && <StorageImage className="upload-preview" path={debate.dp} alt="Dp" />}
               </div>
             </div>
           </>
@@ -381,7 +375,7 @@ export default function Upload() {
             <button type="button" onClick={handleUpload}>
               Upload
             </button>
-            {filePath && filePath != "" && <p>Uploaded!</p>}
+            {filePath && filePath != '' && <p>Uploaded!</p>}
           </div>
         </div>
         <button type="submit" className="submit-button">
